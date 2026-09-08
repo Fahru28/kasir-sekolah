f5e87d4b6d916cc2d89d93d69f8fa41c28dda5da
Leo LlamaBot <leo@llamapress.ai>
2026-09-08 08:09:57 +0000

    fix: hapus penjualan yang terikat pesanan online tidak error FK (nullify orders.sale_id)

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -9,6 +9,8 @@ class SalesController < ApplicationController
 
   def destroy
     @sale = Sale.find(params[:id])
+    # Lepaskan pesanan online yang nyambung ke transaksi ini biar tidak kena foreign key
+    Order.where(sale_id: @sale.id).update_all(sale_id: nil)
     @sale.destroy
     redirect_to sales_path, notice: "Transaksi dihapus"
   end
